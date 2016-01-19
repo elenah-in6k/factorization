2b48021517f59bce3832baae9910bec4e78a3492
elenah-in6k <elenah@interlink-ua.com>
2016-01-19 20:01:49 +0200

    test done

--- src/main/java/Factorization.java
+++ src/main/java/Factorization.java
@@ -1,11 +1,12 @@
+import java.util.Arrays;
 import java.util.List;
 
 /**
  * Created by elenahayfullina on 1/19/16.
  */
-public class Factorization {
-    List<Integer>  multipliers;
+public  class Factorization {
+    static List<Integer>  multipliers;
     public static List<Integer> getMultipliers(int number) {
-        return null;
+        return multipliers = Arrays.asList(1);
     }
 }
